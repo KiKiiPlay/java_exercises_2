88bd5eb2ce3bcc46f8c08485ee1192b4ad8c9165
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-10-08 15:56:50 +0200

    Extra exercise 1 completing

---
+++ extra_tasks.js
@@ -0,0 +1,69 @@
+// Additional assignments for Day 8
+
+/* EXTRA 1
+ Write a piece of code for reverting an array.
+ es:
+ [1, 3, 5] ==> [5, 3, 1]
+*/
+
+console.log("------------------exercise_1-------------------------");
+let normalArray = [1, 2, 3, 4, 5];
+let reversedArray = [];
+for (let i = normalArray.length - 1; i >= 0; i--) {
+  reversedArray.push(normalArray[i]);
+}
+
+console.log(normalArray);
+console.log(reversedArray);
+
+console.log("-----------------------------------------------------\n");
+
+/* EXTRA 2
+ Write a piece of code for getting the maximum numerical value from an array.
+*/
+
+console.log("------------------exercise_2-------------------------");
+console.log("-----------------------------------------------------\n");
+
+/* EXTRA 3
+ Write a piece of code for getting the minimum numerical value from an array.
+*/
+
+console.log("------------------exercise_3-------------------------");
+console.log("-----------------------------------------------------\n");
+
+/* EXTRA 4
+ Write a piece of code for getting only even numerical values from an array.
+*/
+
+console.log("------------------exercise_4-------------------------");
+console.log("-----------------------------------------------------\n");
+
+/* EXTRA 5
+ Write a piece of code for deleting only even entries from an array.
+*/
+
+console.log("------------------exercise_5-------------------------");
+console.log("-----------------------------------------------------\n");
+
+/* EXTRA 6
+ Write a piece of code for removing all the vowels from a string.
+*/
+
+console.log("------------------exercise_6-------------------------");
+console.log("-----------------------------------------------------\n");
+
+/* EXTRA 7
+ Write a piece of code for increasing all the numerical values in a array by 1.
+*/
+
+console.log("------------------exercise_7-------------------------");
+console.log("-----------------------------------------------------\n");
+
+/* EXTRA 8 
+ Replace all the strings contained in an array with their length.
+ es.: ["strive", "is", "great"] => [6, 2, 5]
+*/
+
+console.log("------------------exercise_8-------------------------");
+console.log("-----------------------------------------------------\n");
